23e559f1aad4ed0b9903d3991099e642bbe44f37
sindhya-peter <sindhyapeter@gmail.com>
2025-11-17 22:18:04 -0500

    Add installation script for WSL and Virtual Machine Platform

---
+++ install.ps1
@@ -0,0 +1,3 @@
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart 
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+msiexec.exe /i wsl.msi /qn
