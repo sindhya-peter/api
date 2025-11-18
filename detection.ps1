335c21553cec92b45071c36050dd8a39b7e4fe34
sindhya-peter <sindhyapeter@gmail.com>
2025-11-18 11:11:22 -0500

    Update detection.ps1

--- detection.ps1
+++ detection.ps1
@@ -1,4 +1,6 @@
-$OptionalFeatures = Get-WmiObject -Class Win32_OptionalFeature
-if ( $OptionalFeatures | Where-Object {($_.Name -Match "VirtualMachinePlatform") -and ($_. InstallState -eq 1)} ){
-$true
+if ((wsl --list) -and (Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux).State -eq "Enabled") {
+    Write-Output "WSL Installed"
+    exit 0
+} else {
+    exit 1
 }
